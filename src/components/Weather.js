f16295c3ceebad1757b8cdba7ac27980f5a2c2a7
Efraim Yosofov <efraim224@gmail.com>
2023-05-01 12:23:19 +0300

    changed Weather functional component to class component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,39 +1,53 @@
-import { useEffect, useState } from "react"
-import { WeatherForm } from "./WeatheForm"
 import axios from "axios"
+import React from "react";
 
 
-export const Weather = (props) => {
-    const [city, useCity] = useState('')
-    const [data, setData] = useState('')
+export class Weather extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data : props.data
+        }
 
-    // TODO: find out how to create proper try catch clauses, because the regular not working for fetch. 
-    useEffect(() => {
-            const weather_url = "https://api.openweathermap.org/data/2.5/weather"
-            const params = {'q': city, 'units': 'metric', 'appid': process.env.REACT_APP_WEATHER_KEY}
-            console.log(params)
-            const fetchData = async () => {
-                try {
-                    const result = await axios.get(weather_url,{ params });
-                    const res = await result.data.main.temp
-                    setData(res);
-                } catch (error) {
-                    setData('not valid city')
-                }
+    }
+    
+    componentDidMount() {}
+
+    componentWillUnmount() {}
+
+    setData(value) {
+        this.setState({data: value})
+    }
+    
+    HandleSubmit = (event) => {
+        event.preventDefault();
+        const city = event.target.cityName.value;
+        const weather_url = "https://api.openweathermap.org/data/2.5/weather"
+        const params = {'q': city, 'units': 'metric', 'appid': process.env.REACT_APP_WEATHER_KEY}
+
+        const fetchData = async () => {
+            try {
+                const result = await axios.get(weather_url,{ params });
+                const res = await result.data.main.temp
+                this.setData(res);
+            } catch (error) {
+                this.setData('City not found.')
             }
-            
-            if (city !== '') {
-                fetchData()
-            }
-            else {
-                setData('')
-            }
-        }, [city])
+        }
 
-    return (
-        <div>
-            <WeatherForm useCity={useCity}/>
-            {data}
-        </div>
-    )
+        fetchData()
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.HandleSubmit}>
+                    <label name="fname">City:</label><br></br>
+                    <input type="text" id="cityName"></input>
+                    <input type="submit" value="Submit"></input>
+                </form>
+                {this.state.data}
+            </div>
+        )
+    }
 }
